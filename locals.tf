811459ff6490df7b9d6e35a87aadb8cd940dacbb
Ankit Jain <ankii.jain@gmail.com>
2022-10-15 08:45:12 +0530

    existing policy use enhancement

--- locals.tf
+++ locals.tf
@@ -2,7 +2,8 @@ locals {
     instance_template_name = coalesce(var.launch_template_name, format("%s-template", var.name))
     instance_profile_name = coalesce(var.instance_profile_name, format("%s-instance-profile", var.name))
 
-    instance_policy_names = [for policy in var.instance_profile_policies: policy.name]
+    instance_profile_policy_names = [for policy in var.instance_profile_policies: policy.name]
+    
     instance_profile_roles = [
                         {
                             name = local.instance_profile_name
@@ -11,8 +12,10 @@ locals {
                                 "ec2.amazonaws.com"
                             ]  
                             policy_map = {
-                                policy_arns = local.instance_policy_names
-                            }                   
+                                policy_names = local.instance_profile_policy_names
+                                instance_policy_arns = var.instance_profile_policy_arns
+                            }   
+                                            
                         }
     ]
 }
